ed886013721ad214dcbd091e1fc6f6168b399e16
Valentin Liz <valentin-liz@outlook.com>
2025-11-02 16:29:13 -0300

    Ruleta de partida iniciada terminada

---
+++ public/js/ruleta.js
@@ -0,0 +1,42 @@
+let ruleta;
+function crearRuleta() {
+    const canvas = document.getElementById('ruleta');
+    const tamano = canvas.offsetWidth / 2 - 10;
+
+    ruleta = new Winwheel({
+        'canvasId': 'ruleta',
+        'numSegments': 4,
+        'outerRadius': tamano,
+        'segments': [
+            { 'fillStyle': '#31C950', 'text': 'Ciencia', 'textFillStyle': '#1C1917', 'textFontSize': 22 },
+            { 'fillStyle': '#FF692A', 'text': 'Deportes', 'textFillStyle': '#1C1917', 'textFontSize': 22 },
+            { 'fillStyle': '#FDC745', 'text': 'Historia', 'textFillStyle': '#1C1917', 'textFontSize': 22 },
+            { 'fillStyle': '#51A2FF', 'text': 'Geografía', 'textFillStyle': '#1C1917', 'textFontSize': 22 }
+        ],
+        'animation': {
+            'type': 'spinToStop',
+            'duration': 5,
+            'spins': 8,
+            'callbackFinished': alertResultado
+        }
+    });
+}
+
+// función callback
+function alertResultado(segmento) {
+    const categoria = segmento.text;
+    alert("Salió la categoría: " + categoria);
+}
+
+// botón girar
+document.getElementById('girar').addEventListener('click', () => {
+    ruleta.startAnimation();
+});
+
+// crear ruleta al cargar
+window.addEventListener('load', crearRuleta);
+
+// volver a crear ruleta si cambia el tamaño
+window.addEventListener('resize', () => {
+    crearRuleta();
+});
