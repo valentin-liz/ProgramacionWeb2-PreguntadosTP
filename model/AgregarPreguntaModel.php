a9696b05d7601ce8ba4d61dfb365a734428bea4e
Valentin Liz <valentin-liz@outlook.com>
2025-11-20 23:17:47 -0300

    Agregado de funcionalidad agregarPregunta

--- model/AgregarPreguntaModel.php
+++ model/AgregarPreguntaModel.php
@@ -10,4 +10,38 @@ class AgregarPreguntaModel
         $this->conexion = $conexion;
     }
 
+    public function getCategorias()
+    {
+        $sql = "SELECT id, nombre FROM categoria ORDER BY nombre ASC";
+        $stmt = $this->conexion->prepare($sql);
+        $stmt->execute();
+        return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function agregarPregunta($categoria_id, $pregunta, $opcion_a, $opcion_b, $opcion_c, $opcion_d, $correcta)
+    {
+        $sql = "INSERT INTO preguntas 
+            (categoria_id, pregunta, opcion_a, opcion_b, opcion_c, opcion_d, correcta) 
+            VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+        $stmt = $this->conexion->prepare($sql);
+
+        if (!$stmt) {
+            throw new Exception("Error al preparar la consulta: " . $this->conexion->error);
+        }
+
+        $stmt->bind_param("issssss",
+            $categoria_id,
+            $pregunta,
+            $opcion_a,
+            $opcion_b,
+            $opcion_c,
+            $opcion_d,
+            $correcta
+        );
+
+        return $stmt->execute();
+    }
+
+
 }
