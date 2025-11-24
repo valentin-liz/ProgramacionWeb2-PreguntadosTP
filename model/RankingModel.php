c76ff9ee4cfecd1cbfa6084c43483052ec761b82
SebastianDiaz11 <diaz-sebastian@hotmail.com>
2025-11-23 22:12:19 -0300

    Se agrega QR y nivel de jugador al modal que sale al tocar un jugador

--- model/RankingModel.php
+++ model/RankingModel.php
@@ -12,21 +12,28 @@ class RankingModel
     public function obtenerUsuariosOrdenadosPorRatio() {
 
         $query = "SELECT 
-    id,
-    usuario,
-    puntos,
-    partidas_jugadas,
-    foto_perfil,
-    CASE 
-        WHEN partidas_jugadas > 0 THEN puntos / partidas_jugadas
-        ELSE 0
-    END AS ratio
-FROM usuarios
-ORDER BY ratio DESC;
-";
+            id,
+            usuario,
+            puntos,
+            partidas_jugadas,
+            foto_perfil,
+            CASE 
+                WHEN partidas_jugadas > 0 THEN puntos / partidas_jugadas
+                ELSE 0
+            END AS ratio,
+            CASE
+                WHEN puntos < 10 THEN 1
+                WHEN puntos < 30 THEN 2
+                WHEN puntos < 60 THEN 3
+                WHEN puntos < 100 THEN 4
+                ELSE 5
+            END AS nivel
+        FROM usuarios
+        ORDER BY ratio DESC;
+        ";
+
         $resultado = $this->conexion->query($query);
         return $resultado->fetch_all(MYSQLI_ASSOC);
     }
-
 
 }
